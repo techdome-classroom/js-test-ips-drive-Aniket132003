8a290a12a802891b7c4f4e499bf56caf10263a1d
Aniket Agrawal <aniketagrawal132003@gmail.com>
2024-04-26 13:50:57 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,8 +2,22 @@ function smallestMissingPositiveInteger(nums) {
 
 
     // Implement the function smallest_missing_positive_integer
-  
-  }
-  
-  module.exports = smallestMissingPositiveInteger;
+    nums = nums.filter(num => num > 0);
+    for (let i = 0; i < nums.length; i++) {
+        let pos = Math.abs(nums[i])-1;
+        if (pos < nums.length && nums[pos] > 0) {
+            nums[pos] = -nums[pos];
+        }
+        
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            return i +1
+        }
+        
+    }
+
+}
+
+module.exports = smallestMissingPositiveInteger;
 
